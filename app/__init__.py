ecf8e671e878d1152ad666250cf3769fbd7f91c8
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2021-12-12 18:06:05 -0500

    work

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,54 @@
-from os import urandom 
+from os import MFD_HUGE_SHIFT, urandom 
 from flask import Flask, render_template, request, session, redirect
 import sqlite3, os.path
 
 app = Flask(__name__)
 app.secret_key = urandom(32)
 
+def islogged():
+    return 'username' in session.keys()
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if islogged():
+        return render_template('home.html')
+    else:
+        db = sqlite3.connect("users.db")
+        c = db.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, UNIQUE(username))")
+        db.commit()
+        db.close()
+        return render_template('login.html')
+@app.route("/auth", methods=['GET', 'POST'])
+def auth():
+    if (request.method == 'POST'):
+        db = sqlite3.connect('users.db')
+        c = db.cursor()
+        c.execute("SELECT username AND password FROM users WHERE username=? AND password=?", (request.form['username'], request.form['password']))
+        user = c.fetchone()
+        if (user != None):
+            session['username'] = request.form['username']
+            session['password'] = request.form['password']
+        db.close()
+    return redirect('/')
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if (request.method == 'POST'):
+        db = sqlite3.connect('users.db')
+        c = db.cursor()
+        c.execute("SELECT username AND password FROM users WHERE username=? AND password=?", (request.form['username'], request.form['password']))
+        if (c.fetchone() == None):
+            c.execute("INSERT INTO users(username, password) VALUES(?, ?)", (request.form['username'], request.form['password']))
+            table = "CREATE TABLE {name}(Type TEXT, Object TEXT)".format(name=request.form['username'])
+            c.execute(table)
+        db.commit()
+        db.close()
+        return redirect("/")
+    else:
+        return render_template("register.html")
+@app.route("/trivia", methods=['POST', 'GET'])
+def trivia():
+    return redirect('/')
 
 if __name__ == "__main__":
     app.debug = True
