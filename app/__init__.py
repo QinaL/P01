d6a1ddd0515391529792f6817884abf500e762ed
Yaying <yliangli20@stuy.edu>
2021-12-15 23:32:05 -0500

    devlog

--- app/__init__.py
+++ app/__init__.py
@@ -111,21 +111,6 @@ def trivia():
         print(question)
     return render_template("trivia.html", question=question, correct=correct_answer, incorrect=incorrect_answers)
 
-'''
-@app.route("/")
-def rest_demo():
-    http = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=qsb4nvuGri4tJe3q6REknzJbP5xO1OZnJBDfLKMG") #HTTP Response object (containing the JSON info)
-    print(http)
-    j = json.load(http) #j is a dictionary (key-value pairs) of the JSON info
-    print(j)
-    link = j['url'] #get the value of the 'url' key
-    print(link)
-    explanation = j['explanation'] #get the value of the 'explanation' key
-    print(explanation)
-    #render an html template with the picture (using url) and explanation
-    return render_template("main.html", pic = link, description = explanation)
-'''
-
 if __name__ == "__main__":
     app.debug = True
     app.run()
